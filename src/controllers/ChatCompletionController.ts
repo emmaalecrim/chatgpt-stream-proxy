3658c8e65e62223219ba6c4f5586db24b1095c31
emmaalecrim <emmaalecrim@gmail.com>
2023-06-15 20:15:33 +0100

    secured api key and back to stream cosnumption dev

--- src/controllers/ChatCompletionController.ts
+++ src/controllers/ChatCompletionController.ts
@@ -1,5 +1,6 @@
 import { WebSocket } from "ws";
 import { Configuration, OpenAIApi } from "openai";
+import 'dotenv/config'
 
 import getMessages from "../utils/prompts/helpers";
 
@@ -9,16 +10,18 @@ const ChatCompletionController = async (ws: WebSocket, parsedData: any) => {
         const { messages } = getMessages(type, data)
         // TOOD: move to config file
         const configuration = new Configuration({
-            apiKey: process.env.OPENAI_KEY || "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"
+            apiKey: process.env.OPENAI_KEY
         });
         const openai = new OpenAIApi(configuration);
-        const completion = await openai.createChatCompletion({
+        await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             // @ts-ignore - not sure why this is throwing an error
             messages,
             stream: true,
-        },
-        )
+        }, { responseType: "stream" }).then(function (response) {
+            console.log(response.data.choices)
+        });
+
 
 
     } catch (e) {
